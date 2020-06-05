e16af8a2d35543caed5024684278ac1241559591
Emmagist <emma1994204@gmail.com>
2020-06-05 16:38:33 +0100

    done

---
+++ sub_cat.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="scr/css/style_sub_cat.css">
+  <title>Fixit</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="header">
+      <div class="row ">
+        <div class="col-md-2 mt-2">
+      <img src="img/Fixitlogo transparent.png" alt="">
+        </div>
+        <div class="col-md-7 mt-5">
+      <form class="form-inline my-2 my-lg-0">
+        <input type="search" class="search">
+      </form>
+        </div>
+        <div class="col-md-3 mt-5">
+          <div class="header-div">
+            <a href="login.html" id="header-a">Login</a> <span><a href="signup.html" id="header-a-two">SignUp</a></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="nav">
+      <ul>
+        <li>Graphics & Design</li>
+        <li>Programming & Tech</li>
+        <li>Music & Video</li>
+        <li>Handywork</li>
+        <li>Beauty & Fashion</li>
+        <li>Writing & Translator</li>
+      </ul>
+    </div>
+    <div class="main">
+      <h3>Logo's</h3><span class="main-span"></span>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div><hr>
+            <div class="row">
+              <div class="col-md-6 main-li-div">
+                <ul>
+                  <li class="fa fa-heart-o mr-1 fa-2x" aria-hidden="true"></li>
+                  <li class="fa fa-heart-o fa-2x" aria-hidden="true"></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              <p class="p">Starting At <strong>#5k</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div><hr>
+            <div class="row">
+              <div class="col-md-6 main-li-div">
+                <ul>
+                  <li class="fa fa-heart-o mr-1 fa-2x" aria-hidden="true"></li>
+                  <li class="fa fa-heart-o fa-2x" aria-hidden="true"></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              <p class="p">Starting At <strong>#5k</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div><hr>
+            <div class="row">
+              <div class="col-md-6 main-li-div">
+                <ul>
+                  <li class="fa fa-heart-o mr-1 fa-2x" aria-hidden="true"></li>
+                  <li class="fa fa-heart-o fa-2x" aria-hidden="true"></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              <p class="p">Starting At <strong>#5k</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div><hr>
+            <div class="row">
+              <div class="col-md-6 main-li-div">
+                <ul>
+                  <li class="fa fa-heart-o mr-1 fa-2x" aria-hidden="true"></li>
+                  <li class="fa fa-heart-o fa-2x" aria-hidden="true"></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              <p class="p">Starting At <strong>#5k</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-two mt-4 text-center" id="main-two">
+    <h3 class="mb-3 mt-2">Why you need A Logo For Your Business?</h3>
+      <div class="row text-center">
+        <div class="col-md-6 text-center">
+          <p><span class="span-main-two"><i class="fa fa-check-circle-o mr-3"></i></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odit, ipsa assumenda eligendi, perferendis voluptatem blanditiis perspiciatis rem dolores fugiat dolor, dolore fugit. Perspiciatis veritatis pariatur repellat aliquid nulla vitae.</p>
+          <p><span class="span-main-two"><i class="fa fa-check-circle-o mr-3"></i></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odit, ipsa assumenda eligendi, perferendis voluptatem blanditiis perspiciatis rem dolores fugiat dolor, dolore fugit. Perspiciatis veritatis pariatur repellat aliquid nulla vitae.</p>
+        </div>
+        <div class="col-md-6 text-center">
+        <p><span class="span-main-two"><i class="fa fa-check-circle-o mr-3"></i></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odit, ipsa assumenda eligendi, perferendis voluptatem blanditiis perspiciatis rem dolores fugiat dolor, dolore fugit. Perspiciatis veritatis pariatur repellat aliquid nulla vitae.</p>
+        <p><span class="span-main-two"><i class="fa fa-check-circle-o mr-3"></i></span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non odit, ipsa assumenda eligendi, perferendis voluptatem blanditiis perspiciatis rem dolores fugiat dolor, dolore fugit. Perspiciatis veritatis pariatur repellat aliquid nulla vitae.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
