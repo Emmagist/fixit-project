2e666cbeec76b48f14e015d22f5851bec859df85
Seun <41941546+SeunAkindele@users.noreply.github.com>
2020-06-05 13:37:54 +0100

    seun updated signup.php

---
+++ signup.php
@@ -0,0 +1,131 @@
+<html>
+  <head>
+    <title></title>
+    <link rel="stylesheet" href="scr/css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="scr/css/style_sub_cat.css">
+  </head>
+  <body>
+  <div class="signup-header">
+    
+    <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6 header-signup">
+        <img src="scr/img/Fixitlogo transparent.png" class="logo-transparent" alt="">
+        <input type="text" placeholder="Find Services" class="find-services form-control">
+      </div>
+    
+      <div class="col-md-3"></div>
+      <div class="col-md-3 log-sign">
+        <button class="btn-sign-login">
+          <a href="#" class="login">Login</a>|<a href="#" class="sign-up">Signup</a>
+        </button>
+      </div>
+    </div> 
+    </div>
+  </div>
+  <div class="nav">
+    <div class="container">
+      <ul>
+        <li>Graphics & Designs</li>
+        <li>Programming & Tech</li>
+        <li>Music & Video</li>
+        <li>Handywork</li>
+        <li>Beauty & Fashion</li>
+        <li>Writing & Translation</li>
+      </ul>
+    </div>      
+  </div>
+  <main class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-offset-3 col-lg-12">
+          <div class="title">
+            Welcome To <img src="scr/img/fixitwhite.png" alt="Fixit logo" class="logo-white">
+          </div>
+          
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="intro col-lg-12">
+          We want to get to know you more, please the following informations
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="">First Name</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-group">
+            <label for="">Last Name</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+        <div class="form-group">
+            <label for="">State of Residence</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+        <div class="form-group">
+            <label for="">LGA</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-12">
+        <div class="form-group">
+            <label for="">Residential Address</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+        <div class="form-group">
+            <label for="">Phone  Number</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-6">
+        <div class="form-group">
+            <label for="">Phone Number 2</label>
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="form-group">
+            <label for="">Field of Profession</label>
+            <input type="text" placeholder="You can choose more than word" class="form-control">
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="form-group">
+            <label for="">Description</label>
+            <textarea name="" id="" cols="30" rows="5" class="textarea form-control"></textarea>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <button class="btn btn-upload">Upload A Picture</button>
+        </div>
+        <div class="col-md-3">
+          <label for="file" id="file-label">Choose a file</label>
+          <input type="file" id="file" class="file">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-9 picture-guide">
+          A picture with a clear face, holding a national I.D card or a valid I.D card of respective profession.
+        </div>
+        <div class="col-md-3">
+          <button class="btn btn-upload">Submit</button>
+        </div>
+      </div>
+  </div>
+</main>
+  </body>
+  
+  <script src="src/js/bootstrap.min.js"></script>
+</html>
