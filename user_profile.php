ea5862ef06a7dfacae43aaf54b8728b0236ec36f
Ayenko4all <kenny4all40@gmail.com>
2020-06-06 15:33:17 -0700

    user profile

--- user_profile.php
+++ user_profile.php
@@ -1,36 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="scr/css/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="scr/css/style_sub_cat.css">
-  <title>Document</title>
-</head>
-<body>
+<?php require_once "scr/inc/header2.php"; ?>
   <main class='main'>
-    <div class="container">
-      <div class="flex-container">
-        <div class=" text-success">
-        <p>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
-         </p>
-        </div>
-        <div class="">
-
-         <p>
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
-         </p>
+    <div class="container text-center" id="first-div">
+      <div class="row" >
+        <div class="col-md-8">
+          <div class="pic-area">
+            <img src="scr/img/2.jpg" alt="profile-picute">
+            <div class="mt-5">
+              <p style="margin-bottom: -5%;"> Salami John </p>
+              <a href="#">Top Rated Seller</a>
+              <i class="fa fa-star" id="star-icon">
+                <i class="fa fa-star">
+                <i class="fa fa-star">
+                <i class="fa fa-star">
+                <i class="fa fa-star"><span class="text-dark">(1k+)</span></i>
+                </i>
+                </i>
+                </i>
+              </i>
+            </div>  
+          </div>
+          <div>
+          <button class="btn btn-outline-secondary btnpic mt-3" id="profilebtnupdt1">Change Profile Picture</button>
+            <button class="btn  btnpic" id="profilebtnupdt2">Upgrade To Pro</button>  
+          </div>
           
         </div>
+        <div class="col-md-04">
+          <div class="verified"> <h4 class="text-center">VERIFIED</h4></div>
+        </div>
+      </div>
+    </div>
+  </main>
+  
+  <main class="second-main">
+    <h5>About Seller</h5>
+    <div class="container" id="second-div">
+      
+      <div class="row" id="second-divch2">
+        <div class="col-4 mt-2" style='border-right: 1px solid rgb(226, 225, 225);'>
+          <h6 class="detail text-muted">FROM</h6>
+          <p>Lagos</p>
+        </div>
+        <div class="col-4 mt-2" style='border-right: 1px solid rgb(226, 225, 225);'>
+          <h6 class="detail text-muted">MEMEBER SINCE</h6>
+          <p>July 2012</p>
+        </div>
+        <div class="col-4 mt-2">
+          <h6 class="detail text-muted">Avg Response Time</h6>
+          <p> 15 minute</p>
+        </div>
+      </div>
+      <div class="mt-2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis facilis aut molestiae, suscipit recusandae aspernatur quidem mollitia voluptates id, blanditiis sapiente eaque consequuntur quia iure! Temporibus neque itaque aliquid facere.
+        </p>
       </div>
     </div>
   </main>
   
 
    
-  <script src="src/js/bootstrap.min.js"></script>
-</body>
-</html>
+  <?php require_once "scr/inc/footer.php"; ?>
