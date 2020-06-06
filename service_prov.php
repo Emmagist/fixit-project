a7c007790d8c162a69994ac3eb3dac2466051056
Emmagist <emma1994204@gmail.com>
2020-06-06 23:54:20 +0100

    service provider

---
+++ service_prov.php
@@ -0,0 +1,65 @@
+<?php require_once "scr/inc/header2.php"; ?>
+
+<div class="service_prov">
+  <div class="row mt-5">
+    <div class="col-md-7 main-service-col">
+      <div class="row">
+      <div class="md-8"><h3>Salami John</h3></div>
+      <div class="md-4 ml-5 mt-2"><p>Chat With Seller</p></div>
+      </div>
+    </div>
+    <div class="col-md-4 ml-3">
+      <!-- <div class="job_profile"></div> -->
+      <div class="card" style="width: 23rem;">
+        <div class="card-body main-card" id="job_profile">
+          <div class="row mb-1">
+              <div class="col-md-2"><img src="scr/img/2.jpg" alt="" class="icon-img"></div>
+              <div class="col-md-9 md-4 job-rate"><h4>Salami Toye</h4>
+              <p>Top Rated Seller</p>
+            </div>
+          </div>
+          
+            <p class="card-text">I Will Design Two Modern Logo's.</p>
+            <i class="fa fa-star star-icon fa-2x"></i> <span class="">(1k.+)</span>
+          </div><hr class=" service-prov-hr">
+          <div class="row">
+            <div class="col-md-6 main-li-div">
+              <ul>
+                <li><i class="fa fa-heart mr-1 fa-2x" aria-hidden="true"></i></li>
+                <li><i class="fa fa-heart fa-2x" aria-hidden="true"></i></li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+            <p class="p">Starting At <strong>#5k</strong></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  </div>
+  <div class="container main-service-prov pb-4 pl-5">
+    <div class="job_done">Order Jobs Done By Salami</div>
+    <div class="row">
+      <div class="col-md-4 pt-5">
+        <div class="card" style="width: 18rem;">
+          <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+          <!-- <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div> -->
+        </div>
+      </div>
+      <div class="col-md-4 pt-5">
+        <div class="card" style="width: 18rem;">
+          <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+        </div>
+      </div>
+      <div class="col-md-4 pt-5">
+      <div class="card" style="width: 18rem;">
+          <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require_once "scr/inc/footer.php"; ?>
