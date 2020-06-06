77cf9067a8cb23606221b806cf015006f0795120
Ayenko4all <kenny4all40@gmail.com>
2020-06-06 00:59:27 -0700

    kenny

---
+++ user_profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="scr/css/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="scr/css/style_sub_cat.css">
+  <title>Document</title>
+</head>
+<body>
+  <main class='main'>
+    <div class="container">
+      <div class="flex-container">
+        <div class=" text-success">
+        <p>
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
+         </p>
+        </div>
+        <div class="">
+
+         <p>
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus quisquam numquam tenetur in totam dicta reiciendis, commodi a voluptatem, ex dolores distinctio. Maiores dolor possimus, nemo suscipit quaerat quos unde?
+         </p>
+          
+        </div>
+      </div>
+    </div>
+  </main>
+  
+
+   
+  <script src="src/js/bootstrap.min.js"></script>
+</body>
+</html>
