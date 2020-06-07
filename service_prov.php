8d5a5fae69a2b54ef595fa88e3d21b9c5b5c3d5b
Emmagist <emma1994204@gmail.com>
2020-06-08 00:41:49 +0100

    done

--- service_prov.php
+++ service_prov.php
@@ -1,45 +1,49 @@
 <?php require_once "scr/inc/header2.php"; ?>
 
-<div class="service_prov">
-  <div class="row mt-5">
-    <div class="col-md-7 main-service-col">
-      <div class="row">
-      <div class="md-8"><h3>Salami John</h3></div>
-      <div class="md-4 ml-5 mt-2"><p>Chat With Seller</p></div>
+<div class="service_prov container">
+  <div class="">
+    <div class="row mt-5">
+      <div class="col-md-4 main-service-col">
+        <div class="row main-service-row">
+        <div class="col-md-6"><h3>Salami John</h3></div>
+        <div class="col-md-6 mt-2"><a href="#" id="chat-btn">Chat With Seller</a></div>
+        </div>
       </div>
-    </div>
-    <div class="col-md-4 ml-3">
-      <!-- <div class="job_profile"></div> -->
-      <div class="card" style="width: 23rem;">
-        <div class="card-body main-card" id="job_profile">
-          <div class="row mb-1">
+      <div class="col-md-2"></div>
+      <div class="col-md-2 main-service-card">
+        <!-- <div class="job_profile"></div> -->
+        <div class="card" style="width: 18rem;">
+          <div class="card-body main-card" id="job_profile">
+            <div class="row mb-1">
               <div class="col-md-2"><img src="scr/img/2.jpg" alt="" class="icon-img"></div>
-              <div class="col-md-9 md-4 job-rate"><h4>Salami Toye</h4>
-              <p>Top Rated Seller</p>
+              <div class="col-md-9 job-rate">
+                <h4>Salami Toye</h4>
+                <p>Top Rated Seller</p>
+              </div>
             </div>
-          </div>
-          
-            <p class="card-text">I Will Design Two Modern Logo's.</p>
-            <i class="fa fa-star star-icon fa-2x"></i> <span class="">(1k.+)</span>
-          </div><hr class=" service-prov-hr">
-          <div class="row">
-            <div class="col-md-6 main-li-div">
-              <ul>
-                <li><i class="fa fa-heart mr-1 fa-2x" aria-hidden="true"></i></li>
-                <li><i class="fa fa-heart fa-2x" aria-hidden="true"></i></li>
-              </ul>
-            </div>
-            <div class="col-md-6">
-            <p class="p">Starting At <strong>#5k</strong></p>
+            
+              <p class="card-text">I Will Design Two Modern Logo's.</p>
+              <i class="fa fa-star star-icon fa-2x"></i> <span class="">(1k.+)</span>
+            </div><hr class=" service-prov-hr">
+            <div class="row">
+              <div class="col-md-6 main-li-div">
+                <ul>
+                  <li><i class="fa fa-heart mr-1 fa-2x" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-heart fa-2x" aria-hidden="true"></i></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              <p class="p">Starting At <strong>#5k</strong></p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    </div>
   </div>
-  <div class="container main-service-prov pb-4 pl-5">
+  <div class=" main-service-prov pl-3">
     <div class="job_done">Order Jobs Done By Salami</div>
-    <div class="row">
+    <div class="row   pb-4 pl-2">
       <div class="col-md-4 pt-5">
         <div class="card" style="width: 18rem;">
           <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
@@ -58,6 +62,12 @@
           <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
         </div>
       </div>
+    </div>
+  </div>
+  <div>
+    <div class="row main-service-prov" id="main-two-service-prov">
+      <div class="colo-md-2"><img src="scr/img/2.jpg" alt="" class="icon-img"></div>
+      <div class="colo-md-10"></div>
     </div>
   </div>
 </div>
