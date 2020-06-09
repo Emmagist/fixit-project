649e62905fbf7bbb7e5a992228b5dfdf07e46041
Emmagist <emma1994204@gmail.com>
2020-06-09 11:31:26 +0100

    sevice provider

--- service_prov.php
+++ service_prov.php
@@ -23,13 +23,13 @@
             </div>
             
               <p class="card-text">I Will Design Two Modern Logo's.</p>
-              <i class="fa fa-star star-icon fa-2x"></i> <span class="">(1k.+)</span>
+              <i class="fa fa-star star-icon"></i> <span class="">(1k.+)</span>
             </div><hr class=" service-prov-hr">
             <div class="row">
               <div class="col-md-6 main-li-div">
                 <ul>
-                  <li><i class="fa fa-heart mr-1 fa-2x" aria-hidden="true"></i></li>
-                  <li><i class="fa fa-heart fa-2x" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-heart mr-1 f-1x" aria-hidden="true"></i></li>
+                  <li><i class="fa fa-heart fa-1x" aria-hidden="true"></i></li>
                 </ul>
               </div>
               <div class="col-md-6">
@@ -58,8 +58,8 @@
         </div>
       </div>
       <div class="col-md-4 pt-5">
-      <div class="card" style="width: 18rem;">
-          <img src="scr/img/slide1.jpg" class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem;">
+          <img src="scr/img/slide1.jpg" class="card-img-top" alt="..." width="70%">
         </div>
       </div>
     </div>
@@ -69,7 +69,7 @@
     <div class="row" id="main-two-service-prov">
       <div class="col-md-1"><img src="scr/img/2.jpg" alt="" class="icon-img-two"></div>
       <div class="col-md-11 pl-3">
-      <div class=""><i class="fa fa-star fa-2x service-star"> <span><i class="fa fa-star"></i></span> <span><i class="fa fa-star"></i></span></i></div>
+      <div class=""><i class="fa fa-star service-star"> <span><i class="fa fa-star"></i></span> <span><i class="fa fa-star"></i></span></i></div>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus maxime iusto facere, omnis repudiandae eligendi perferendis. Quia asperiores hic nam nostrum cupiditate possimus enim ea quaerat sequi, accusantium veniam itaque.</p>
       <div class=""><i class="fa fa-thumbs-o-up mr-3 helpfull-icon"> Helpful</i> <span><i class="fa fa-thumbs-o-down helpfull-icon"> Not Helpful</i></span></div>
       </div>
@@ -77,7 +77,7 @@
     <div class="row  pt-4" id="main-two-service-prov">
       <div class="col-md-1"><img src="scr/img/2.jpg" alt="" class="icon-img-two"></div>
       <div class="col-md-11 pl-3">
-      <div class=""><i class="fa fa-star fa-2x service-star"> <span><i class="fa fa-star"></i></span></i></div>
+      <div class=""><i class="fa fa-star service-star"> <span><i class="fa fa-star"></i></span></i></div>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus maxime iusto facere, omnis repudiandae eligendi perferendis. Quia asperiores hic nam nostrum cupiditate possimus enim ea quaerat sequi, accusantium veniam itaque.</p>
       <div class=""><i class="fa fa-thumbs-o-up mr-3 helpfull-icon"> Helpful</i> <span><i class="fa fa-thumbs-o-down helpfull-icon"> Not Helpful</i></span></div>
       </div>
@@ -87,24 +87,19 @@
     <div class="col-md-3">
       <img src="scr/img/2.jpg" alt="" class="icon-img-three ml-1"><br><br>
       <a href="#" id="chat-btn">Chat With Seller</a>
-  </div>
-    <div class="col-md-6 text-center">
+    </div>
+    <div class="col-md-6 text-center container">
       <h3>About Seller</h3>
-      <table class="table ">
-        <thead class="table-bordered">
-          <tr>
-            <th scope="col">From <br> <span>Lagos</span></th>
-            <th scope="col">Member<br><span>Since August 2020</span></th>
-            <th scope="col">Avg. Response Time<br><span>15 Mins</span></th>
-          </tr>
-        </thead>
-        <!-- <tbody> -->
-          
-            <p class="table-bordered mt-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque inventore suscipit itaque, ad nulla numquam ullam accusantium. Accusamus ipsa aut, officiis qui ratione porro voluptatum enim veritatis sunt odit atque?</p>
-          <!-- </td> -->
-          <!-- <tr> -->
-        <!-- </tbody> -->
-      </table>
+      <div class="">
+        <div class="row table_bordered">
+          <div class="col-md-4 table_bord pt-2 pb-2">From <br><span>Lagos</span></div>
+          <div class="col-md-4 table_bord pt-2 pb-2">Member<br><span>Since August 2020</span></div>
+          <div class="col-md-4 table_bordere pt-2 pb-2">Avg. Response Time<br><span>15 Mins</span></div>
+        </div>
+        <div class="row text-center" id="table_bordered">
+          <p class="p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus tempore similique delectus quae, ipsa qui ratione iure enim et voluptates. Doloremque voluptatibus hic praesentium. Et at aliquid reprehenderit eaque iste.</p>
+        </div>
+      </div>
     </div>
     <div class="col-md-3"></div>
   </div>
