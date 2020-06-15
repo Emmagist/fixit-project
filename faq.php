af789525ae5c4ac00f183cedf8819d84ffc02dc0
Emmagist <emma1994204@gmail.com>
2020-06-15 17:56:44 +0100

    faq.php updated

--- faq.php
+++ faq.php
@@ -1,13 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <div class="container-text-center">
-    <div class="col-one"></div>
+<?php require_once "scr/inc/header1.php"; ?>
+
+  <div class="container">
+    <h3 class="text-center mt-5" style="font-size: 24px; font-weight: bolder;">Frequently Asked Questions</h3>
+    <p class="text-center mb-5" style="font-size: 18px; font-weight: bold;">We Have Answers To Your Possible Questions</p>
+    <div class="row">
+      <div class="col-md-9 col-one mt-5"id="faq-container" >
+        <div class="row">
+          <div class="col-md-1">
+            <span class="faq-col-span"><img src="scr/img/yellow.jpg" alt=""></span>
+          </div>
+          <div class="col-md-10">
+            <h5 style="font-size: 18px; margin-left: -50px;">Lorem Ipsoum</h5>
+          </div>
+        </div>
+        <p class="" style="font-size: 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum corporis, sequi at tenetur expedita ad itaque maxime laboriosam saepe quas earum vel nostrum qui amet, molestias libero. Dolores, repudiandae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci suscipit enim at nisi explicabo, est maiores possimus libero tempore fuga doloremque, odio esse atque odit, ea iste necessitatibus itaque fugiat. <br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatem molestias velit totam repellat! Consectetur, quae omnis explicabo ullam magnam praesentium exercitationem hic vitae qui totam necessitatibus impedit. Veniam, voluptate? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit dolorum quam ullam itaque ad voluptas similique aut delectus magnam magni? Dolore nostrum at sequi voluptate enim repellendus adipisci explicabo nobis.</p>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-9 col-one mt-5"id="faq-container" >
+        <div class="row">
+          <div class="col-md-1">
+            <span class="faq-col-span"><img src="scr/img/yellow.jpg" alt=""></span>
+          </div>
+          <div class="col-md-10">
+            <h5 style="font-size: 18px; margin-left: -50px;">Lorem Ipsoum</h5>
+          </div>
+        </div>
+        <p class="" style="font-size: 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum corporis, sequi at tenetur expedita ad itaque maxime laboriosam saepe quas earum vel nostrum qui amet, molestias libero. Dolores, repudiandae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci suscipit enim at nisi explicabo, est maiores possimus libero tempore fuga doloremque, odio esse atque odit, ea iste necessitatibus itaque fugiat. <br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatem molestias velit totam repellat! Consectetur, quae omnis explicabo ullam magnam praesentium exercitationem hic vitae qui totam necessitatibus impedit. Veniam, voluptate? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit dolorum quam ullam itaque ad voluptas similique aut delectus magnam magni? Dolore nostrum at sequi voluptate enim repellendus adipisci explicabo nobis.</p>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-9 col-one mt-5"id="faq-container" >
+        <div class="row">
+          <div class="col-md-1">
+            <span class="faq-col-span"><img src="scr/img/yellow.jpg" alt=""></span>
+          </div>
+          <div class="col-md-10">
+            <h5 style="font-size: 18px; margin-left: -50px;">Lorem Ipsoum</h5>
+          </div>
+        </div>
+        <p class="" style="font-size: 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum corporis, sequi at tenetur expedita ad itaque maxime laboriosam saepe quas earum vel nostrum qui amet, molestias libero. Dolores, repudiandae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci suscipit enim at nisi explicabo, est maiores possimus libero tempore fuga doloremque, odio esse atque odit, ea iste necessitatibus itaque fugiat. <br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatem molestias velit totam repellat! Consectetur, quae omnis explicabo ullam magnam praesentium exercitationem hic vitae qui totam necessitatibus impedit. Veniam, voluptate? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit dolorum quam ullam itaque ad voluptas similique aut delectus magnam magni? Dolore nostrum at sequi voluptate enim repellendus adipisci explicabo nobis.</p>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+    <div class="row">
+      <div class="col-md-9 col-one mt-5"id="faq-container" >
+        <div class="row">
+          <div class="col-md-1">
+            <span class="faq-col-span"><img src="scr/img/yellow.jpg" alt=""></span>
+          </div>
+          <div class="col-md-10">
+            <h5 style="font-size: 18px; margin-left: -50px;">Lorem Ipsoum</h5>
+          </div>
+        </div>
+        <p class="" style="font-size: 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatum corporis, sequi at tenetur expedita ad itaque maxime laboriosam saepe quas earum vel nostrum qui amet, molestias libero. Dolores, repudiandae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci suscipit enim at nisi explicabo, est maiores possimus libero tempore fuga doloremque, odio esse atque odit, ea iste necessitatibus itaque fugiat. <br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe voluptatem molestias velit totam repellat! Consectetur, quae omnis explicabo ullam magnam praesentium exercitationem hic vitae qui totam necessitatibus impedit. Veniam, voluptate? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit dolorum quam ullam itaque ad voluptas similique aut delectus magnam magni? Dolore nostrum at sequi voluptate enim repellendus adipisci explicabo nobis.</p>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
   </div>
-</body>
-</html>
+  
+<?php require_once "scr/inc/footer.php"; ?>
