6e1987b619f667b1744357b92a9a7e51de710666
Emmagist <emma1994204@gmail.com>
2020-06-16 15:27:31 +0100

    first signup page updated

--- signup.php
+++ signup.php
@@ -1,94 +1,43 @@
-<html>
-  <?php require_once "scr/inc/header2.php"; ?>
-  <main class="main">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="title">
-            Welcome To <img src="scr/img/fixitwhite.png" alt="Fixit logo" class="logo-white">
-          </div>
-          
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fixit</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="scr/css/bootstrap.min.css">
+  <link rel="stylesheet" href="scr/css/style_sub_cat.css">
+</head>
+<body style="background: rgb(39, 2, 2);">
+  <section class="container mt-3">
+    <form action="" class="form-group" id="form">
+      <h4 class="form-h4 text-center mt-5">Join <span><img src="scr/img/fixitwhite.png" alt=""></span></h4>
+      <div class="" id="form-one">
+        <input type="text" class="form-control" placeholder="Example@gmail.com">
       </div>
 
-      <div class="row">
-        <div class="intro col-lg-12">
-          We want to get to know you more, please the following informations
-        </div>
+      <div class="form-two mt-5">
+        <h4 class="form-two-h4">Continue As</h4>
+        <input type="text" class="form-control mb-4" placeholder="Service Employer">
+        <!-- <span><hr> <p>OR</p> <hr></span> -->
+        
       </div>
-
-      <div class="row">
-        <div class="col-md-6">
-          <div class="form-group">
-            <label for="">First Name</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="form-group">
-            <label for="">Last Name</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-6">
-        <div class="form-group">
-            <label for="">State of Residence</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-6">
-        <div class="form-group">
-            <label for="">LGA</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-12">
-        <div class="form-group">
-            <label for="">Residential Address</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-6">
-        <div class="form-group">
-            <label for="">Phone  Number</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-6">
-        <div class="form-group">
-            <label for="">Phone Number 2</label>
-            <input type="text" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="form-group">
-            <label for="">Field of Profession</label>
-            <input type="text" placeholder="You can choose more than word" class="form-control">
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="form-group">
-            <label for="">Description</label>
-            <textarea name="" id="" cols="30" rows="5" class="textarea form-control"></textarea>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <button class="btn btn-upload">Upload A Picture</button>
-        </div>
-        <div class="col-md-3">
-          <label for="file" id="file-label">Choose a file</label>
-          <input type="file" id="file" class="file">
-        </div>
+      <div class="container col-container">
+      <div class="form-three row">
+        <div class="col-md-3 form-col mb-4 mt-2" id="form-col"></div>
+        <div class="col-md-1 col-form mb-3">or</div>
+        <div class="col-md-3 form-col mb-4 mt-2"></div>
       </div>
-      <div class="row">
-        <div class="col-md-9 picture-guide">
-          A picture with a clear face, holding a national I.D card or a valid I.D card of respective profession.
-        </div>
-        <div class="col-md-3">
-          <button class="btn btn-upload">Submit</button>
-        </div>
       </div>
-  </div>
-</main>
-
-<?php require_once "scr/inc/footer.php"; ?>
+      <div class="form-two">
+      <input type="text" class="form-control mb-4" placeholder="Service Provider">
+        <button class="btn btn-primary form-control main-signup">SignIn</button>
+      </div>
+      <div class="footer text-center mt-5">
+        <p class="form-p">By joining I agree to receive mails from <span><img src="scr/img/fixitwhite.png" alt=""></span></p>
+        <p>Already a member? <a href="#" class="text-primary">SignIn</a></p>
+      </div>
+    </form>
+  </section>
+</body>
+</html>
