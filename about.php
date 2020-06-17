869ba69e4d94214c6dc75f157d46206f18a6cd42
Seun <41941546+SeunAkindele@users.noreply.github.com>
2020-06-17 17:08:15 +0100

    updating terms.php

---
+++ about.php
@@ -0,0 +1,82 @@
+<html>
+  <?php require_once "scr/inc/header1.php"; ?>
+  <main class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="title-about">
+          <h1>ABOUT US</h1>
+            <b>KNOW ABOUT WHAT WE REPRESENT AND HOW IT STARTED</b>
+          </div>          
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <div class="paragraph">
+            <span><i class="fa fa-check-circle-o about_span-color"></i></span> <b>WHAT IS LOREM IPSUM</b>
+          </div>
+        </div>
+        <div class="col-md-10 about-col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, voluptas suscipit fugiat facere quos alias vero quam! Exercitationem iste similique sapiente adipisci suscipit dicta dolorem vero laboriosam, aliquid neque saepe!
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-10 about-col">
+          MAKE A TYPE SPECIMEN
+        </div>
+      </div>
+
+      <div class="row fig-container">
+        <div class="col-md-1 perfect-middle"></div>
+        <div class="col-md-3">
+          <figure class="fig_shape">
+            <img src="scr/img/1.jpeg" alt="" class="img_shape">
+          </figure>
+          <p class="paragraph">WHAT IS LOREM ISPUM<br>CR</p>
+        </div>
+        <div class="col-md-1 perfect-middle">
+          <div class="paragraph">
+            <span><i class="fa fa-check-circle-o about_span-color about_span-size"></i></span>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <figure class="fig_shape">
+            <img src="scr/img/1.jpeg" alt="" class="img_shape">
+          </figure>
+        </div>
+        <div class="col-md-1 perfect-middle">
+          <div class="paragraph">
+            <span><i class="fa fa-check-circle-o about_span-color about_span-size"></i></span>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <figure class="fig_shape">
+            <img src="scr/img/1.jpeg" alt="" class="img_shape">
+          </figure>
+        </div>
+      </div>
+
+      <div class="row">
+      <div class="col-md-12">
+          <div class="paragraph">
+            <span><i class="fa fa-check-circle-o about_span-color"></i></span> <b>WHAT IS LOREM IPSUM</b>
+          </div>
+        </div>
+        <div class="col-md-10 about-col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, voluptas suscipit fugiat facere quos alias vero quam! Exercitationem iste similique sapiente adipisci suscipit dicta dolorem vero laboriosam, aliquid neque saepe!
+        </div>
+        <div class="col-md-12">
+          <div class="paragraph">
+            <span><i class="fa fa-check-circle-o about_span-color"></i></span> <b>WHAT IS LOREM IPSUM</b>
+          </div>
+        </div>
+        <div class="col-md-10 about-col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, voluptas suscipit fugiat facere quos alias vero quam! Exercitationem iste similique sapiente adipisci suscipit dicta dolorem vero laboriosam, aliquid neque saepe!
+        </div>
+      </div>
+  
+    </div>
+</main>
+<?php require_once "scr/inc/footer.php"; ?>
