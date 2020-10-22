ace82de08ff516026f6af045c58b0cc03b46a600
Ogundipe Olusegun <emma1994204@gmail.com>
2020-10-22 18:52:10 +0100

    about page

--- about.php
+++ about.php
@@ -1,4 +1,43 @@
- <?php require_once "scr/inc/header1.php"; ?>
+<?php
+    require "controllers/init.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <title>Fixit</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="scr/css/bootstrap.min.css">
+    <link rel="stylesheet" href="scr/css/index.css">
+    <link rel="stylesheet" href="scr/css/footer.css">
+  </head>
+<body>
+ <div class="container" id="index-loginbtn">
+     <?php
+     if (!$fun->ifLoggedIn()):?>
+         <a href="login.php" class="btn">SIGN IN</a>
+         <a href="signup.php" class="btn">SIGN UP</a>
+     <?php endif;?>
+
+ </div>
+ <div class="nav">
+    <div class="container">
+      <ul>
+        <?php foreach(User::navCategory() as $nav) : ?>
+        <?php 
+            // $id = $nav['id'];
+            // $subnav = User::subNavCategory($id);
+
+            // foreach($subnav as sub) :
+        ?>
+        <li>
+            <a href="sub_cat.php?sub=<?=$nav['id']?>"><?=$nav['categories']?></a>
+        </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>      
+  </div>
+    
   <main class="main">
     <div class="container">
       <div class="row">
